6a77080a62231950dedffde57e33a7eff5aee52a
Kumar Rohit <93335681+k-Rohit@users.noreply.github.com>
2025-08-08 17:09:52 +0530

    chaning main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8501"],
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
